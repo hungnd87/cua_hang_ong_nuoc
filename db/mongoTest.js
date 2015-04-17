99a074190762f83c0992e0a1e7cd45e80955def4
Hung <ilsh2001@gmail.com>
2015-04-17 21:46:22 +0700

    update layout - customer ideals

--- db/mongoTest.js
+++ db/mongoTest.js
@@ -1,1 +1,60 @@
-var properties = require('../config/properties.js');
+﻿var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://125.212.202.232:27017/htmlresource';
+MongoClient.connect(url, function(err, db) {
+  console.log(err)
+  console.log("Connected correctly to server");
+  findDocuments(db, function(){
+  	db.close();	
+  })
+  
+});
+
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('sanpham');
+  // Insert some documents
+  collection.insert(
+    [{
+    	type: 'ppr_wavin_ekoplastik',
+    	name: 'Nhà phân phối'
+    }]
+  , function(err, result) {
+	console.log(result);  
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('sanpham');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    console.log(docs)
+    console.dir(docs);
+    callback(docs);
+  });
+}
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.update({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
+
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.remove({ a : 3 }, function(err, result) {
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });    
+}
+
+
